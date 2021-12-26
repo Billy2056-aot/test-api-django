dd9356d9e09a352249004b7cf0bb4599b06e8666
PC-Billy <tulakarn.2557@gmail.com>
2021-12-26 16:41:56 +0700

    commit

--- manage.py
+++ manage.py
@@ -1,10 +1,10 @@
 
-from django.core import management
-from django.core.management.commands import loaddata
+# from django.core import management
+# from django.core.management.commands import loaddata
 
-management.call_command('flush', verbosity=0, interactive=False)
-management.call_command('loaddata', 'test_data', verbosity=0)
-management.call_command(loaddata.Command(), 'test_data', verbosity=0)
+# management.call_command('flush', verbosity=0, interactive=False)
+# management.call_command('loaddata', 'test_data', verbosity=0)
+# management.call_command(loaddata.Command(), 'test_data', verbosity=0)
 
 
 
