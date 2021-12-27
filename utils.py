479ebf8db29b9af7e290f8d8861f5579cf535e6f
PC-Billy <tulakarn.2557@gmail.com>
2021-12-27 16:12:26 +0700

    commit

--- utils.py
+++ utils.py
@@ -650,24 +650,25 @@ def is_valid_cidr(string_network):
 
 
 @contextlib.contextmanager
-
-def set_environ(env_name, value):
+def set_environ(env_name, value): 
     """Set the environment variable 'env_name' to 'value'
     Save previous value, yield, and then restore the previous value stored in
     the environment variable 'env_name'.
     If 'value' is None, do nothing"""
     value_changed = value is not None
+    type (set_environ.old_value)
+    
     if value_changed:
         
         old_value = os.environ.get(env_name)
         os.environ[env_name] = value
-
+        
     try:
         
         yield 
     finally: 
         if value_changed:
-            
+                        
             if old_value is None:
                 del os.environ[env_name]
             else:
