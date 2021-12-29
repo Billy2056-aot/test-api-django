000fa299c722801c42c84a93f0686b8b91cb81a3
PC-Billy <tulakarn.2557@gmail.com>
2021-12-29 17:07:13 +0700

    commit

--- certs.py
+++ certs.py
@@ -12,7 +12,7 @@ If you are packaging Requests, e.g., for a Linux distribution or a managed
 environment, you can change the definition of where() to return a separately
 packaged CA bundle.
 """
-from certifi import where
+
 from _pydecimal import __name__
 import requests 
 requests.get('https://github.com' , verify=True)
